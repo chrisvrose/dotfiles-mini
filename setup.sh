8a0fb60a037912630e7e3d4e6fc85b5425899d78
Christopher Rose <christophervrose@gmail.com>
2021-04-08 19:57:36 +0530

    [add] termite+i3status

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
 test ! -d ~/.config/sway && ln -s "$(pwd)/sway" ~/.config/sway
+
+test ! -d ~/.config/i3status && ln -s "$(pwd)/i3status" ~/.config/i3status
+
+test ! -d ~/.config/termite && ln -s "$(pwd)/termite" ~/.config/termite
