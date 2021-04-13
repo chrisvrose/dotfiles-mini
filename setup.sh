b4834645a4ed5c917b9be12c175a68a71724bcf7
Christopher Rose <christophervrose@gmail.com>
2021-04-13 13:40:42 +0530

    [add] wallpaper

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ test ! -d ~/.config/sway && ln -s "$(pwd)/sway" ~/.config/sway
 test ! -d ~/.config/i3status && ln -s "$(pwd)/i3status" ~/.config/i3status
 
 test ! -d ~/.config/termite && ln -s "$(pwd)/termite" ~/.config/termite
+
+test ! -d ~/.config/wallpaper && ln -s "$(pwd)/wallpaper" ~/.config/wallpaper
